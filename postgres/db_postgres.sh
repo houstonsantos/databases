ff689ed4aae5a43315769f4005cd30b429d0c4ec
Houston Santos <houston_santos@hotmail.com>
2024-06-01 22:26:11 -0300

    porjeto databases

---
+++ postgres/db_postgres.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+# Criação da database para o protheus, postgres
+mkdir /var/lib/postgresql/data/protheus
+chown postgres:postgres /var/lib/postgresql/data/protheus
+
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE ROLE "db_protheus" WITH
+	    NOSUPERUSER 
+	    CREATEDB 
+	    NOCREATEROLE
+	    REPLICATION
+	    NOINHERIT;
+	
+    CREATE ROLE $POSTGRES_USER_TOTVS LOGIN PASSWORD '$POSTGRES_PASS_TOTVS'
+	    NOSUPERUSER 
+	    CREATEDB 
+	    NOCREATEROLE
+	    REPLICATION
+	    NOINHERIT;
+	
+    GRANT "db_protheus" TO $POSTGRES_USER_TOTVS;
+	
+	CREATE TABLESPACE db_protheus OWNER $POSTGRES_USER_TOTVS LOCATION '/var/lib/postgresql/data/protheus/';
+    
+    CREATE DATABASE $DATABASE WITH
+		OWNER = '$POSTGRES_USER_TOTVS'
+		ENCODING = 'WIN1252'
+    	LC_COLLATE = 'C'
+    	LC_CTYPE = 'C'
+    	TABLESPACE = db_protheus
+		IS_TEMPLATE = False
+    	CONNECTION LIMIT = -1;
+EOSQL
